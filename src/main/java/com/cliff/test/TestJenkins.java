7df1ee5e0e6cd59c806030031393c4f38c8c94a8
Clifford Dias <diasc@Cliffords-MacBook-Pro.local>
2017-07-23 17:25:35 +0100

    initial commit

---
+++ src/main/java/com/cliff/test/TestJenkins.java
@@ -0,0 +1,17 @@
+package com.cliff.test;
+
+public class TestJenkins {
+
+	public static void main(String[] args){
+		
+		TestJenkins test = new TestJenkins();
+		
+		int onePlusOne = test.onePlusOne(); 
+		
+		System.out.println("One Plus One is = " + onePlusOne);
+	}
+	
+	public int onePlusOne(){
+		return 1+1;
+	}
+}
